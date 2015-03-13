6e1f30fab8f62147329b687e48f0a0b76b36414a
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-13 19:09:22 -0300

    Updating...

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var azk_core    = require('./lib/src/azk-core');
-var config      = require('./lib/src/config');
-var i18n        = require('./lib/src/i18n');
-var utils       = require('./lib/src/utils');
 var spec_helper = require('./lib/spec/spec_helper');
 
 module.exports = {
   __esModule: true,
-  // get azk_core    : azk_core,
-  // spec_helper : spec_helper,
-  get config      () { return config; },
-  get i18n        () { return i18n; },
-  get utils       () { return utils; },
-
-  // fast access to utils functions
-  get _           () { return azk_core._; },
-  get h           () { return spec_helper; },
+  get h() { return spec_helper; },
+  get version() { return azk_core.version; },
+  get Q() { return azk_core.Q; },
+  get _() { return azk_core._; },
+  get lazy_require() { return azk_core.lazy_require; },
+  get t() { return azk_core.t; },
+  get config() { return azk_core.config; },
+  get set_config() { return azk_core.set_config; },
+  get defer() { return azk_core.defer; },
+  get async() { return azk_core.async; },
+  get os() { return azk_core.os; },
+  get path() { return azk_core.path; },
+  get fs() { return azk_core.fs; },
+  get isBlank() { return azk_core.isBlank; },
+  get log() { return azk_core.log; },
 };
