eee815275065a878cbc6147b89d413cc47ab09d7
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-13 18:56:25 -0300

    Updating...

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var utils       = require('./lib/src/utils');
 var spec_helper = require('./lib/spec/spec_helper');
 
 module.exports = {
-	azk_core    : azk_core,
-	spec_helper : spec_helper,
-	config      : config,
-	i18n        : i18n,
-	utils       : utils,
+  __esModule: true,
+  // get azk_core    : azk_core,
+  // spec_helper : spec_helper,
+  get config      () { return config; },
+  get i18n        () { return i18n; },
+  get utils       () { return utils; },
 
-	// fast access to utils functions
-	_           : azk_core._,
-	h           : spec_helper,
+  // fast access to utils functions
+  get _           () { return azk_core._; },
+  get h           () { return spec_helper; },
 };
