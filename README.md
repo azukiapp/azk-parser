afebc2dfbed56c346fd87e2052a732298ea8cf7d
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-13 18:42:34 -0300

    Updating index.js

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # azk-core
 
+## npm dependencies
+
+- babel              (https://github.com/babel/babel)
+- colors             (https://github.com/Marak/colors.js)
+- crypto             (https://github.com/Gozala/crypto)
+- lodash             (https://github.com/lodash/lodash)
+- printf             (https://github.com/wdavidw/node-printf)
+- q                  (https://www.npmjs.com/package/q)
+- source-map-support (https://github.com/evanw/node-source-map-support)
+
+
 #### Compile and watch
 
 ```
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+var azk_core    = require('./lib/src/azk-core');
+var config      = require('./lib/src/config');
+var i18n        = require('./lib/src/i18n');
+var utils       = require('./lib/src/utils');
+var spec_helper = require('./lib/spec/spec_helper');
+
 module.exports = {
-	'azk-core': require('./lib/src/azk-core'),
-	'config'  : require('./lib/src/config'),
-	'i18n'    : require('./lib/src/i18n'),
-	'utils'   : require('./lib/src/utils')
+	_           : azk_core._,
+	spec_helper : spec_helper,
+	config      : config,
+	i18n        : i18n,
+	utils       : utils,
 };
