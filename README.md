91a94464d3e3f18a231feb961ae4d256f74e04aa
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-16 13:53:14 -0300

    Creating a verbose option

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # azk-projects-boilerplate
 
-## npm dependencies
-
-- babel              (https://github.com/babel/babel)
-- colors             (https://github.com/Marak/colors.js)
-- crypto             (https://github.com/Gozala/crypto)
-- lodash             (https://github.com/lodash/lodash)
-- printf             (https://github.com/wdavidw/node-printf)
-- q                  (https://www.npmjs.com/package/q)
-- source-map-support (https://github.com/evanw/node-source-map-support)
-
+`azk-projects-boilerplate` follow `azk` standards to create new npm packages.
 
 #### Compile and watch
 
--- bin/getMax.js
+++ bin/getMax.js
@@ -2,20 +2,22 @@
 
 var _ = require('azk-core')._;
 var SampleClass = require('../lib/src/sample-class');
-var util = require('util');
-var inspect = function (data) {
-  console.log('   ' + util.inspect(data, { showHidden:false, colors:true }));
-};
+var sampleClass = new SampleClass();
 
 // https://www.npmjs.com/package/nopt
 var nopt     = require('nopt');
 
 var knownOpts  =  {
-                    'number' : [Number, Array]
+                    'number' : [Number, Array],
+                    'help'   : Boolean,
+                    'verbose'   : Boolean,
                   };
 
 var shortHands =  {
                     'n' : ['--number'],
+                    '?' : ['--help'],
+                    'h' : ['--help'],
+                    'v' : ['--verbose'],
                   };
 
              // everything is optional.
@@ -24,13 +26,43 @@ var shortHands =  {
              // slice defaults to 2
 var parsed     = nopt(knownOpts, shortHands, process.argv, 2);
 
+if (parsed.help || parsed.argv.original.length === 0) {
+  var usage_message = ['',
+    ' Usage:',
+    '',
+    '   $ getMax [options]',
+    '',
+    ' Options:',
+    '',
+    '   --number, -n     include a number to find max',
+    '   --verbose, -v    show more info on output',
+    '   --help, -h, -?   show this help message',
+    '',
+    ' Examples: (all lines below will return 5)',
+    '',
+    '   $ getMax -n 1 -n 2 -n 3 -n 4 -n 5',
+    '   $ getMax --number 1 --number 2 --number 3 --number 4 --number 5',
+    '   $ getMax 1 2 3 4 5',
+    '   $ getMax 1 -n 2 3 --number 4 5',
+    ''].join('\n');
+  console.log(usage_message);
+  return;
+}
+
 var remain_numbers;
 var all_numbers;
-var sampleClass = new SampleClass();
-var result;
 
-console.log('\n parsed nopt args:');
-inspect(parsed);
+var result;
+var is_verbose = parsed.verbose || false;
+
+// verbose
+var util = require('util');
+var inspect = function (data) {
+  console.log('   ' + util.inspect(data, { showHidden:false, colors:true }));
+};
+
+is_verbose && console.log('\n parsed nopt args:');
+is_verbose && inspect(parsed);
 
 // remain: convert strings to numbers
 if (_.isArray(parsed.argv.remain)) {
@@ -39,18 +71,18 @@ if (_.isArray(parsed.argv.remain)) {
   });
 }
 
-if (_.isArray(parsed.numbers) && _.isArray(remain_numbers)) {
-  all_numbers = parsed.numbers.concat(remain_numbers);
-} else if (_.isArray(parsed.numbers)) {
-  all_numbers = parsed.numbers;
+if (_.isArray(parsed.number) && _.isArray(remain_numbers)) {
+  all_numbers = parsed.number.concat(remain_numbers);
+} else if (_.isArray(parsed.number)) {
+  all_numbers = parsed.number;
 } else if (_.isArray(remain_numbers)) {
   all_numbers = remain_numbers;
 }
 
 result = sampleClass.getMaxNumber(all_numbers);
 
-console.log('\n all_numbers:');
-inspect(all_numbers);
+is_verbose && console.log('\n all_numbers:');
+is_verbose && inspect(all_numbers);
 
-console.log('\n sampleClass.getMaxNumber(all_numbers):');
-inspect(result);
+is_verbose && console.log('\n sampleClass.getMaxNumber(all_numbers) -->');
+console.log(result);
