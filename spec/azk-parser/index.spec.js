c25c116d6522214ebcf44645e0efbcec71c35ce2
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-24 16:33:38 -0300

    new node-example Azkfile.js

--- spec/azk-parser/index.spec.js
+++ spec/azk-parser/index.spec.js
@@ -6,4 +6,16 @@ describe('AzkParser:', function() {
     azkParser = new AzkParser();
   });
 
+  it('should parse an Azkfile.js', function () {
+    var systems = azkParser.getSystemsFromAzkfile('./Azkfile.js');
+    var system001 = new System({ name: 'system001' });
+    systems.addSystem(system001);
+    systems.removeSystem();
+
+    var system001 = systems.find('system001');
+    system001.depends('system002');
+
+    azkParser.saveSystemsToAzkfile(systems, './Azkfile.js');
+  });
+
 });
