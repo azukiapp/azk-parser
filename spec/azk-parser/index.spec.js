734e875c65e0f86053a817e287e06cabc8aee595
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-24 22:25:00 -0300

    Testing genereted Azkfile

--- spec/azk-parser/index.spec.js
+++ spec/azk-parser/index.spec.js
@@ -1,5 +1,8 @@
 import h         from '../spec-helper';
 import AzkParser from '../../src/azk-parser';
+import fileUtils from '../../src/file-utils';
+import Parser    from '../../src/parser';
+var parser = new Parser();
 // import System    from '../../src/azk-parser/syntaxes/system';
 var bb = require('bluebird');
 var spawn = bb.coroutine;
@@ -20,13 +23,23 @@ describe('AzkParser:', function() {
 
   it('should generate an Azkfile.js', function () {
     return spawn(function* () {
+      // get systems
       var node_example_path = './fixtures/azkfile-examples/node-example-Azkfile.js';
       var systems = yield azkParser.getSystemsFromAzkfile(node_example_path);
+
+      // get ast
       var ast = systems.convert_to_ast;
 
+      // generate code
       azkParser.saveSystemsToAzkfile(ast, '/tmp/node-example-Azkfile.js');
 
-      h.expect(ast).to.not.be.undefined;
+      // check generation
+      var generated_content = yield fileUtils.read('/tmp/node-example-Azkfile.js');
+      var generatade_ast = parser.parse(generated_content.toString()).syntax;
+      var properties_ast = generatade_ast.program.body[0].expression.arguments[0].properties;
+      /**/require('azk-core').dlog(properties_ast, "properties_ast", 2);/*-debug-*/
+
+      h.expect(properties_ast).to.have.length(1);
     })();
   });
 
